ad3d94884d71b8761d33f54f5c962d78f252d5aa
SiddharthBansal007 <bansal.siddharth21@gmail.com>
2024-08-26 11:22:20 +0530

    updated frontend files and added navbar

--- src/App.js
+++ src/App.js
@@ -14,8 +14,21 @@ import config from './config.json'
 
 function App() {
 
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
   return (
     <div>
+      <Navigation account = {account} setAccount={setAccount} />
 
       <h2>Welcome to Dappazon</h2>
 
